68f63eb67178daaaedc34001b2bf890463007efe
Elie Roux <elie.roux@telecom-bretagne.eu>
2018-03-25 11:03:49 +0200

    update

--- pillow/README.md
+++ pillow/README.md
@@ -2,7 +2,6 @@
 
 The goal here is to check the possibility to write Tibetan in an image with Pillow.
 
-https://github.com/HOST-Oman/libraqm
+Pillow needs [libraqm](https://github.com/HOST-Oman/libraqm) in order to work, but as stated on the [installation page](https://pillow.readthedocs.io/en/latest/installation.html): `Raqm support is currently unsupported on Windows.`.
 
-Current result:
-
+See [this bug report](https://github.com/python-pillow/Pillow/issues/3054)
--- pillow/testpillow.py
+++ pillow/testpillow.py
@@ -2,21 +2,26 @@ import os
 from PIL import Image, ImageDraw, ImageFont
 
 # Draw dummy image
-img = Image.new('RGB', (800, 100), color = (73, 109, 137))
+img = Image.new('RGB', (800, 300), color = (73, 109, 137))
 d = ImageDraw.Draw(img)
 
 # Load font list
 fontfile = 'NotoSansTibetan-Regular.ttf'
-fonts_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'fonts')
 
-# Print font name and tests
+# simple layout engine:
 y = 10
 fnt = ImageFont.truetype(fontfile, 24)
 d.text((10, y), "༄༅། །སྒྲུབ།", font=fnt, fill=(255, 255, 0))
 y +=  50
 
+# with libraqm
 fnt = ImageFont.truetype(fontfile, 24, layout_engine=ImageFont.LAYOUT_RAQM)
 d.text((10, y), "༄༅། །སྒྲུབ།", font=fnt, fill=(255, 255, 0))
 y +=  50
+
+# using opentype features:
+# list from https://docs.microsoft.com/en-us/typography/script-development/tibetan
+fnt = ImageFont.truetype('NotoSansTibetan-Regular.ttf', 24, layout_engine=ImageFont.LAYOUT_RAQM)
+res = fnt.getmask("༄༅། །སྒྲུབ།", features=['ccmp', 'abvs', 'blws', 'calt', 'liga', 'kern', 'abvm', 'blwm', 'mkmk'])
 
 img.save('result.png')
